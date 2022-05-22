9b13ba4aa9b3fc2dc0d8dd6902ebf64ed26fbcf1
Madan Chaudhary <madan712@gmail.com>
2022-05-22 19:22:31 +0100

    shell888

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 echo "--- :building_construction: Builing.."
 
 echo "Values inside script..."
-KK=$(buildkite-aget meta-data get "MYKEY")
-VV=$(buildkite-aget meta-data get "MYVALUE")
+KK=$(buildkite-agent meta-data get "MYKEY")
+VV=$(buildkite-agent meta-data get "MYVALUE")
 echo $KK
 echo $VV
 
