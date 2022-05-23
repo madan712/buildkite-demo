e93c8612ca6d6da4a7af619daba2927199da7cf9
Madan Chaudhary <madan712@gmail.com>
2022-05-23 15:23:28 +0100

    eureka

--- src/component/public.jsx
+++ src/component/public.jsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 export default function Public() {
 	return (
 		<Container fluid className='bg-light text-dark p-5'>
-			<h2>Public Page is working fine</h2>
+			<h2>Public Page</h2>
 		</Container>
 	);
 }
